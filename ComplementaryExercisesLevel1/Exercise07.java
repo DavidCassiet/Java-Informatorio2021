eb771514ec1c77dea480af7ce40c6cd97d07e089
DavidCassiet <davidleonelcassiet@hotmail.com>
2021-10-19 15:34:03 -0300

    optimizado

--- ComplementaryExercisesLevel1/Exercise07.java
+++ ComplementaryExercisesLevel1/Exercise07.java
@@ -3,7 +3,16 @@ import java.util.Scanner;
 public class Exercise07 {
     
     public static void main(String[] args) {
-        
-    }
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter lowercase string :");
+        String string = scanner.nextLine();
+        
+        char[] newString = new char[string.length()];
+        
+        for(int i = 0; i < string.length(); i++) {
+            newString[i] = (char) (string.charAt(i) - 32);
+        } 
+        System.out.println(newString);
+    }
 }
